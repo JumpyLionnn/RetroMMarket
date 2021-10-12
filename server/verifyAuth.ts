c4ab45002ebf2bae1021414abb926177197ad05c
fwipsity <fwipsity@gmail.com>
2021-10-12 14:28:15 +0300

    made the jwt token expire after 3 hours

--- server/verifyAuth.ts
+++ server/verifyAuth.ts
@@ -5,6 +5,10 @@ function verifyAuth(req: ExpressRequest, res: ExpressResponse, next: () => void)
     }
     try{
         const verified = jwt.verify(token, process.env.JWT_SECRET);
+        // make sure the token has not expired(expire time: 3 hours)
+        if(Math.floor(Date.now() / 1000) - verified.iat > 60 * 60 * 3){ 
+            throw new Error();
+        }
         req.user = verified;
         next();
     }
