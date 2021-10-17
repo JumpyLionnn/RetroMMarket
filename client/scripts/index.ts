933558966c4fdc75422760936849a9586d5a0a33
jpasikainen <asikainen.juhapekka@gmail.com>
2021-10-17 22:01:56 +0300

    Fetch fix

--- client/scripts/index.ts
+++ client/scripts/index.ts
@@ -17,6 +17,7 @@ interface Item {
 class ItemCard extends HTMLDivElement {
     constructor() {
         super();
+        // TODO: Link onlclick here
     }
 
     connectedCallback() {
@@ -34,12 +35,12 @@ class ItemCard extends HTMLDivElement {
             <div id="item-details">
                 <div id="item-count-selector">
                     <div>Item count:</div>
-                    <input type="number" value="1" id="{{item.sellOrderId}}" min="1" max="{{item.count}}">
+                    <input class="item-count-number-input" type="number" value="1" id="" min="1" max="1">
                 </div>
-                <div id="price-number">{{item.price}}</div>
+                <div id="price-number"></div>
             </div>
             <div id="buy-button-container">
-                <button id="buy-button" value="{{item.sellOrderId}}" onclick="buyItem(this.value)">BUY NOW</button>
+                <button id="buy-button" value="" onclick="buyItem(this.value)">BUY NOW</button>
             </div>
         </div>
         `;
@@ -50,48 +51,47 @@ customElements.define("item-card", ItemCard, { extends: "div" });
 
 async function getSellOffers(query: string, category: string, onlineSellersOnly: boolean, sortBy: sortTypes, upperLimit: number) {
     const url = `/find?query=${query}&category=${category}&onlineSellersOnly=${onlineSellersOnly}&sort=${sortBy}&to=${upperLimit}`;
-    const data = {
-        query: query,
-        category: category,
-        onlineSellersOnly: onlineSellersOnly,
-        sort: sortBy,
-        to: upperLimit
-    };
+    console.log(url);
     const res = await fetch(url, {
         method: "GET",
-        headers: {"Content-Type": "application/json"}
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
     });
-    console.log(res)
-    return res.json().then(data => {
-        data.map((item: any) => {
-            const wasd = {
+
+    if(!res.ok) throw new Error("error")
+
+    return await res.json();
+}
+
+async function renderItems(query: string, category: string, onlineSellersOnly: boolean, sortBy: sortTypes, upperLimit: number) {
+    const items: [] | undefined = await getSellOffers(query, category, onlineSellersOnly, sortBy, upperLimit);
+    console.log(items);
+    const fragment = document.createElement("div", { is: "item-card" });
+    items?.map((item: any) => {
+        return {
             image: item.image,
             itemName: item.itemName,
             category: item.category,
             seller: item.retrommousername,
-            sellerStatus: "",               // TODO: implement
-            sellOrderId: item.id, // just id?
+            sellerStatus: (onlineSellersOnly) ? "online" : "offline",
+            sellOrderId: item.id,
             itemCount: item.amount,
             price: item.price
-            }
-            console.log(wasd);
-            return wasd
-        })
-    })
-}
-
-function renderItems(query: string, category: string, onlineSellersOnly: boolean, sortBy: sortTypes, upperLimit: number) {
-    const items = getSellOffers(query, category, onlineSellersOnly, sortBy, upperLimit) as any;
-    console.log(items);
-    const fragment = document.createElement("div", { is: "item-card" });
-    
-    items.forEach((item: Item) => {
+        }
+    });
+    items?.forEach((item: Item) => {
         const instance = document.importNode(fragment, true);
         (<HTMLImageElement>instance.querySelector("#item-image")).src = item.image;
         (<HTMLSpanElement>instance.querySelector("#item-name")).innerHTML = item.itemName;
         (<HTMLSpanElement>instance.querySelector("#category-name")).innerHTML = item.category;
         (<HTMLSpanElement>instance.querySelector("#seller-name")).innerHTML = item.seller;
         (<HTMLSpanElement>instance.querySelector("#seller-status")).style.color = (item.sellerStatus === "online") ? "green" : "red";
+        (<HTMLInputElement>instance.querySelector(".item-count-number-input")).max = item.itemCount.toString();
+        (<HTMLInputElement>instance.querySelector(".item-count-number-input")).id = item.sellOrderId.toString();
+        (<HTMLDivElement>instance.querySelector("#price-number")).innerHTML = item.price.toString();
+        (<HTMLButtonElement>instance.querySelector("#buy-button")).value = item.sellOrderId.toString();
 
         document.getElementById("items-list")?.appendChild(instance);
     })
