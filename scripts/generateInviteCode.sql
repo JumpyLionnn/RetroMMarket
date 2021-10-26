65e6963e8a78c8f55cb851c14b0fdb34617b0773
fwipsity <fwipsity@gmail.com>
2021-10-26 21:10:43 +0300

    added a script to generate invite tokens

---
+++ scripts/generateInviteCode.sql
@@ -0,0 +1,12 @@
+do
+$do$
+declare
+     i int;
+begin
+for  i in 1..100
+loop
+    INSERT INTO invitationTokens (token) VALUES (substr(md5(random()::text), 0, 7));
+end loop;
+end;
+$do$;
+SELECT token FROM invitationTokens;
