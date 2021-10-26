3e2c5c54333beb1f462ac27c358584d58ceae227
fwipsity <fwipsity@gmail.com>
2021-10-26 21:24:18 +0300

    fixed a bug that caused done orders to show up

--- server/database/buyOrders/getBuyOrdersBySellOfferId.ts
+++ server/database/buyOrders/getBuyOrdersBySellOfferId.ts
@@ -1,6 +1,6 @@
 async function getBuyOrdersBySellOfferId(sellOfferId: number){
     return (await client.query(`SELECT buyOrders.id, buyerid, amount, sellerdelivered, buyerdelivered, retrommousername, discordname
      FROM buyOrders, users
-     WHERE sellOfferId = $1 AND users.id = buyerId;
+     WHERE sellOfferId = $1 AND users.id = buyerId AND buyOrders.done = false;
      `, [sellOfferId])).rows;
 }
