de0bc82476f686fd6a205d22237703d8851b9c0d
fwipsity <fwipsity@gmail.com>
2021-11-06 19:19:18 +0200

    changed input names for confirming passwords

--- scripts/update.sql
+++ scripts/update.sql
@@ -2,13 +2,13 @@ ALTER TABLE users ADD COLUMN emailVerified BOOLEAN DEFAULT false;
 ALTER TABLE users ADD COLUMN admin BOOLEAN DEFAULT false;
 ALTER TABLE users ADD COLUMN banned BOOLEAN DEFAULT false;
 
-ALTER TABLE buyOrders DROP CONSTRAINT sellOfferId;
+ALTER TABLE buyOrders DROP CONSTRAINT buyorders_sellofferid_fkey;
 ALTER TABLE buyOrders ADD FOREIGN KEY (sellOfferId) REFERENCES sellOffers (id) on delete cascade;
 
-ALTER TABLE buyOrders DROP CONSTRAINT buyerId;
+ALTER TABLE buyOrders DROP CONSTRAINT buyorders_buyerid_fkey;
 ALTER TABLE buyOrders ADD FOREIGN KEY (buyerId) REFERENCES users(id) on delete cascade;
 
-ALTER TABLE sellOffers DROP CONSTRAINT sellerid;
+ALTER TABLE sellOffers DROP CONSTRAINT selloffers_sellerid_fkey;
 ALTER TABLE sellOffers ADD FOREIGN KEY (sellerId) REFERENCES users(id) on delete cascade;
 
 DELETE FROM sellOffers WHERE canceled = true;
